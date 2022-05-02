9b8562bc8c45f1dbf353a8077a8527003b5da240
brajeshkumar1989 <brajesh071989@gmail.com>
2022-05-02 21:16:59 +0530

    changed timeout_sec to 30 from 10

--- src/js/model.js
+++ src/js/model.js
@@ -36,8 +36,7 @@ export const loadRecipe = async function (id) {
     state.recipe = createRecipeObject(data);
     // console.log(res, data);
 
-    if (state.bookmarks.some(bookmark => bookmark.id === id))
-      state.recipe.bookmarked = true;
+    if (state.bookmarks.some(bookmark => bookmark.id === id)) state.recipe.bookmarked = true;
     else state.recipe.bookmarked = false;
 
     // console.log(recipe);
