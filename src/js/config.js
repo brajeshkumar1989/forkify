2f04fff075617a5c8071dca88cb6b34bc96491f2
BRAJESH KUMAR <brajesh071989@gmail.com>
2024-05-02 15:13:33 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 30;
 export const RES_PER_PAGE = 10;
-export const KEY = '839bb0f4-72d9-455b-b777-b433f5f97562';
+export const KEY = '839bb0f4-72d9-455b-b777-b433f5f9756';
 export const MODEL_CLOSE_SEC=2.5
